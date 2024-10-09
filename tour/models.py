5fb84bd07580325336b6dee5a012e22ebf95defc
Ved <145837583+vedd0623@users.noreply.github.com>
2024-10-09 10:50:50 +0530

    contact-- models.py

--- tour/models.py
+++ tour/models.py
@@ -60,12 +60,4 @@ class Contact(models.Model):
     def __str__(self):
         return self.first_Name
 
-class Blog(models.Model):
-    user= models.ForeignKey(User,on_delete=models.CASCADE,null=True,blank=True)
-    topic=models.CharField(max_length=100,null=True,blank=True)
-    image= models.ImageField(blank=True)
-    blog= RichTextField(blank=True,null=True)
-    post_date= models.CharField(max_length=30,null=True,blank=True)
 
-    def __str__(self):
-        return self.topic
